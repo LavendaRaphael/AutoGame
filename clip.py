b481a0e1883f66def86bd4beb37c09948027f5e8
LavendaRaphael <feifei.tian.cn@gmail.com>
2025-07-06 12:15:22 +0800

    improve starrail

--- clip.py
+++ clip.py
@@ -2,13 +2,18 @@ import cv2
 import numpy as np
 def main():
     pic_list = [
+        'cap/20250622_131628.png',
+        'cap/20250622_131754.png',
         'cap/20250622_131842.png',
+        'cap/20250622_132124.png',
         'cap/20250622_132135.png',
         'cap/20250622_132239.png',
+        'cap/20250705_205634.png',
         'cap/20250705_211809.png',
+        'cap/20250706_110019.png',
     ]
-    x1, y1 = 1845, 1027
-    x2, y2 = 1898, 1077
+    x1, y1 = 1885, 1150
+    x2, y2 = 2020, 1200
 
     image = cv2.imread(pic_list[0], cv2.IMREAD_UNCHANGED)
     diff = np.zeros(image.shape[:2])
