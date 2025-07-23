e5e6fe6881dc31d58d6206fdc4b71e62a2db0e3d
LavendaRaphael <feifei.tian.cn@gmail.com>
2025-07-23 23:15:32 +0800

    improve zzz

--- auto_cv.py
+++ auto_cv.py
@@ -25,14 +25,15 @@ def ZZZ(hwnd, log_overlay, pic_overlay):
             break
         if is_key_pressed(";"):
             pic_list = [
-                {'pic':'ZZZ/dialog_2140_1130_2220_1230.png', "picxy": (2140,1130), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
-                {'pic':'ZZZ/dialog_2140_1230_2220_1330.png', "picxy": (2140,1248), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
-                {'pic':'ZZZ/dialog_582_1404_860_1728.png'  , "picxy": (582 ,1404), 'picwh': (278,324), 'key': 'SPACE'    ,"shift": (100,0)},
-                {'pic':'ZZZ/dialog_2140_1230_2220_1330.png', "picxy": (2140,1230), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
-                {'pic':'ZZZ/dialog_582_1404_860_1728.png'  , "picxy": (582 ,1386), 'picwh': (278,324), 'key': 'SPACE'    ,"shift": (100,0)},
-                {'pic':'ZZZ/dialog_810_772_890_840.png'    , "picxy": (810 ,772 ), 'picwh': (80 ,162), 'key': '1'        ,"shift": (100,0)},
-                {'pic':'ZZZ/dialog_810_772_890_840.png'    , "picxy": (810 ,678 ), 'picwh': (80 ,162), 'key': '1'        ,"shift": (100,0)},
-                {'pic':'ZZZ/dialog_2472_258_2746_322.png'  , "picxy": (2472,258 ), 'picwh': (274,64 ), 'key': 'SPACE'    ,"shift": (100,0)},
+                {'pic':'ZZZ/dialog_2140_1130_2220_1230_1.png', "picxy": (2140,1130), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
+                {'pic':'ZZZ/dialog_2140_1130_2220_1230.png'  , "picxy": (2140,1130), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
+                {'pic':'ZZZ/dialog_2140_1230_2220_1330.png'  , "picxy": (2140,1248), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
+                {'pic':'ZZZ/dialog_582_1404_860_1728.png'    , "picxy": (582 ,1404), 'picwh': (278,324), 'key': 'SPACE'    ,"shift": (100,0)},
+                {'pic':'ZZZ/dialog_2140_1230_2220_1330.png'  , "picxy": (2140,1230), 'picwh': (80 ,100), 'key': 'MOUSELEFT',"shift": (100,50)},
+                {'pic':'ZZZ/dialog_582_1404_860_1728.png'    , "picxy": (582 ,1386), 'picwh': (278,324), 'key': 'SPACE'    ,"shift": (100,0)},
+                {'pic':'ZZZ/dialog_810_772_890_840.png'      , "picxy": (810 ,772 ), 'picwh': (80 ,162), 'key': '1'        ,"shift": (100,0)},
+                {'pic':'ZZZ/dialog_810_772_890_840.png'      , "picxy": (810 ,678 ), 'picwh': (80 ,162), 'key': '1'        ,"shift": (100,0)},
+                {'pic':'ZZZ/dialog_2472_258_2746_322.png'    , "picxy": (2472,258 ), 'picwh': (274,64 ), 'key': 'SPACE'    ,"shift": (100,0)},
             ]
             skipping_cv(log_overlay, pic_overlay, hwnd, pic_list)
         elif is_key_pressed("["):
@@ -41,8 +42,8 @@ def ZZZ(hwnd, log_overlay, pic_overlay):
 
 def Nikky(hwnd, log_overlay, pic_overlay):
     _, active_window = get_window_title(hwnd)
-    txt = f"{active_window} ; 跳过"
-    log_overlay.update_title(txt)
+    title = f"{active_window} ; 跳过"
+    log_overlay.update_title(title)
     while True:
         hwnd_x, active_window = get_window_title()
         if hwnd_x != hwnd:
@@ -52,13 +53,12 @@ def Nikky(hwnd, log_overlay, pic_overlay):
                 {'pic':'InfinityNikky/dialog_0_0_450_136.png', "picrange": (0,0,450,136), 'key': 'F', "shift": (100,0)},
             ]
             skipping_cv(log_overlay, pic_overlay, hwnd, pic_list)
-            log_overlay.update_text(txt)
         time.sleep(0.2)
 
 def StarRail(hwnd, log_overlay, pic_overlay):
     _, active_window = get_window_title(hwnd)
-    txt = f"{active_window} ; 跳过"
-    log_overlay.update_title(txt)
+    title = f"{active_window} ; 跳过"
+    log_overlay.update_title(title)
     while True:
         hwnd_x, active_window = get_window_title()
         if hwnd_x != hwnd:
@@ -69,13 +69,12 @@ def StarRail(hwnd, log_overlay, pic_overlay):
                 {'pic':'StarRail/dialog_192_52_450_120.png'     , "picrange": (192,   52, 450, 120), 'key': 'SPACE', "shift": (-100,0)},
             ]
             skipping_cv(log_overlay, pic_overlay, hwnd, pic_list)
-            log_overlay.update_text(txt)
         time.sleep(0.2)
 
 def Reverse1999(hwnd, log_overlay, pic_overlay):
     _, active_window = get_window_title(hwnd)
-    txt = f"{active_window} ; 跳过"
-    log_overlay.update_title(txt)
+    title = f"{active_window} ; 跳过"
+    log_overlay.update_title(title)
     while True:
         hwnd_x, active_window = get_window_title()
         if hwnd_x != hwnd:
@@ -85,7 +84,6 @@ def Reverse1999(hwnd, log_overlay, pic_overlay):
                 {'pic':'Reverse1999/dialog_2314_162_2766_236.png', "picrange": (2314,162,2766,236), 'key': 'SPACE', "shift": (-100,0)},
             ]
             skipping_cv(log_overlay, pic_overlay, hwnd, pic_list)
-            log_overlay.update_text(txt)
         time.sleep(0.2)
     
 def game_script_thread(log_overlay, pic_overlay):
